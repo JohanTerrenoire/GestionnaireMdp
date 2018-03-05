609f57b5a11758bdc9dfdb1edf89fb3f29d471e7
JohanTerrenoire <terrenoirejohan@yahoo.fr>
2018-03-05 20:27:37 +0100

    Tentative Authentification

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 use Auth;
 
 class LoginController extends Controller
@@ -48,13 +50,39 @@ class LoginController extends Controller
     //Soumettre le formulaire de connexion
     protected function postLogin(Request $request)
     {
-      // Récupérer les infos du formulaire puis hasher le mot de passe pour le comparer
-      $this->validate($request, [
-      $this->username() => 'required', 'password' => 'required',
-      ]);
+      // validate the info, create rules for the inputs
+      $rules = array(
+          'email'    => 'required|email', // make sure the email is an actual email
+          'password' => 'required|alphaNum|min:3' // password can only be alphanumeric and has to be greater than 3 characters
+      );
+
+      // run the validation rules on the inputs from the form
+      $validator = Validator::make(
+        array(
+            'password' => $request['password'],
+            'email' => $request['email']
+        ), $rules);
+      if ($validator->fails()) {
+        return Redirect::to('login')
+        ->withErrors($validator) // send back all errors to the login form
+        ->withInput(Input::except('password')); // send back the input (not the password) so that we can repopulate the form
+      } else {
+        // create our user data for the authentication
+        $userdata = array(
+        'email'     => $request['email'],
+        'password'  => $request['password']
+      );
     }
-    public function username()
-    {
-      return 'email';
+    // attempt to do the login
+    if (Auth::attempt($userdata)) {
+      // validation successful!
+      // redirect them to the secure section or whatever
+      // return Redirect::to('secure');
+      // for now we'll just echo success (even though echoing in a controller is bad)
+      return 'SUCCESS!';
+    } else {
+      // validation not successful, send back to form
+      return Redirect::to('user/login');
     }
+  }
 }
